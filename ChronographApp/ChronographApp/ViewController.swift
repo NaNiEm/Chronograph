773696a672bfafbe027dc5ee0322c59dd916e97f
NancyGomez <nagomez@csumb.edu>
2018-03-13 16:17:43 -0700

    Added project setup with google pods

---
+++ ChronographApp/ChronographApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChronographApp
+//
+//  Created by Nancy Gomez on 3/13/18.
+//  Copyright © 2018 Nancy Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
