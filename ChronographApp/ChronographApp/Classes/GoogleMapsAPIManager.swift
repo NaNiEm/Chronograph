5cd2b9834a2ab18f9c1f3e7e665724efdcd3663a
EmilyGarcia <emily1597@gmail.com>
2018-04-10 17:42:52 -0700

    Calling directions API, bug: must call API in main thread

--- ChronographApp/ChronographApp/Classes/GoogleMapsAPIManager.swift
+++ ChronographApp/ChronographApp/Classes/GoogleMapsAPIManager.swift
@@ -10,5 +10,73 @@ import Foundation
 import GoogleMaps
 
 class GoogleMapsAPIManager {
-
+//    var session: URLSession
+//
+//    init() {
+//        session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+//    }
+//
+//    func getPolylineRoute(from source: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D){
+//
+//        let url = URL(string:"https://maps.googleapis.com/maps/api/directions/json?origin=\(source.latitude),\(source.longitude)&destination=\(destination.latitude),\(destination.longitude)&sensor=true&mode=driving&key=AIzaSyCsiXLRfeicvlewhtRuHf0VnTNf4g5InLE")!
+//
+//        let task = session.dataTask(with: url, completionHandler: {
+//            (data, response, error) in
+//            if error != nil {
+//                completion(nil, error)
+////                print(error!.localizedDescription)
+////                self.activityIndicator.stopAnimating()
+//            }
+//            else {
+//                do {
+//                    if let json : [String:Any] = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]{
+//
+//                        guard let routes = json["routes"] as? NSArray else {
+//                            DispatchQueue.main.async {
+////                                self.activityIndicator.stopAnimating()
+//                            }
+//                            return
+//                        }
+//                        completion(routes, nil)
+//                        if (routes.count > 0) {
+//                            let overview_polyline = routes[0] as? NSDictionary
+//                            let dictPolyline = overview_polyline?["overview_polyline"] as? NSDictionary
+//
+//                            let points = dictPolyline?.object(forKey: "points") as? String
+//
+//                            self.showPath(polyStr: points!)
+//
+//                            DispatchQueue.main.async {
+////                                self.activityIndicator.stopAnimating()
+//
+//                                let bounds = GMSCoordinateBounds(coordinate: source, coordinate: destination)
+//                                let update = GMSCameraUpdate.fit(bounds, with: UIEdgeInsetsMake(170, 30, 30, 30))
+//                                self.mapView!.moveCamera(update)
+//                            }
+//                        }
+//                        else {
+//                            DispatchQueue.main.async {
+////                                self.activityIndicator.stopAnimating()
+//                            }
+//                        }
+//                    }
+//                }
+//                catch {
+//                    print("error in JSONSerialization")
+//                    DispatchQueue.main.async {
+////                        self.activityIndicator.stopAnimating()
+//                    }
+//                }
+//            }
+//        })
+//        task.resume()
+//    }
+//
+//    func showPath(polyStr :String){
+//        let path = GMSPath(fromEncodedPath: polyStr)
+//        let polyline = GMSPolyline(path: path)
+//        polyline.strokeWidth = 3.0
+//        polyline.strokeColor = UIColor.red
+//        polyline.map = mapView // Your map view
+//    }
 }
--- ChronographApp/ChronographApp/ViewControllers/InitialViewController.swift
+++ ChronographApp/ChronographApp/ViewControllers/InitialViewController.swift
@@ -22,11 +22,12 @@ class InitialViewController: UIViewController, GMSMapViewDelegate {
     var stationName: String = ""
     @IBOutlet weak var SetDestView: UIView!
     var stations: [Station] = []
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBOutlet weak var mapUIView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        activityIndicator.startAnimating()
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
@@ -48,7 +49,12 @@ class InitialViewController: UIViewController, GMSMapViewDelegate {
         mapView.isHidden = true
         mapView.delegate = self
         fetchBartList()
-        
+        let path = GMSMutablePath()
+        path.add(CLLocationCoordinate2D(latitude: -33.85, longitude: 151.20))
+        path.add(CLLocationCoordinate2D(latitude: -33.70, longitude: 151.40))
+        path.add(CLLocationCoordinate2D(latitude: -33.73, longitude: 151.41))
+        let polyline = GMSPolyline(path: path)
+        polyline.map = mapView
     }
     func plotStations(){
         for station in stations {
@@ -125,10 +131,87 @@ class InitialViewController: UIViewController, GMSMapViewDelegate {
         for station in stations{
             if(stationName == station.name){
                 setGeoFence(station: station)
+                let destination = CLLocationCoordinate2D(latitude: CLLocationDegrees(station.latitude), longitude: CLLocationDegrees(station.longitude))
+                let source = defaultLocation.coordinate
+                print("printing source and destination")
+                print(source)
+                print(destination)
+                getPolylineRoute(from: source, to: destination)
             }
         }
         
     }
+    
+    func getPolylineRoute(from source: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D){
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let url = URL(string: "https://maps.googleapis.com/maps/api/directions/json?origin=\(source.latitude),\(source.longitude)&destination=\(destination.latitude),\(destination.longitude)&sensor=true&mode=transit&key=AIzaSyAR-HPSnLwn4pdl_KjzNvuJ7KmWkM4yoDY")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+                self.activityIndicator.stopAnimating()
+            }
+            else {
+                do {
+                    if let json : [String:Any] = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]{
+                        
+                        print(json)
+                        guard let routes = json["routes"] as? NSArray else {
+                            DispatchQueue.main.async {
+                                self.activityIndicator.stopAnimating()
+                            }
+                            return
+                        }
+                        print("printing routes")
+                        print(routes)
+                        if (routes.count > 0) {
+                            let overview_polyline = routes[0] as? NSDictionary
+                            let dictPolyline = overview_polyline?["overview_polyline"] as? NSDictionary
+                            
+                            let points = dictPolyline?.object(forKey: "points") as? String
+                            print("printing points")
+                            print(points!)
+                            self.showPath(polyStr: points!)
+                            
+                            DispatchQueue.main.async {
+//                                self.activityIndicator.stopAnimating()
+                                
+                                let bounds = GMSCoordinateBounds(coordinate: source, coordinate: destination)
+                                let update = GMSCameraUpdate.fit(bounds, with: UIEdgeInsetsMake(170, 30, 30, 30))
+                                self.mapView!.moveCamera(update)
+                            }
+                        }
+                        else {
+                            DispatchQueue.main.async {
+                                self.activityIndicator.stopAnimating()
+                            }
+                        }
+                    }
+                }
+                catch {
+                    print("error in JSONSerialization")
+                    DispatchQueue.main.async {
+                        self.activityIndicator.stopAnimating()
+                    }
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    func showPath(polyStr :String){
+        let path = GMSPath(fromEncodedPath: polyStr)
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeWidth = 10.0
+        polyline.strokeColor = UIColor.red
+        polyline.map = mapView // Your map view
+    }
+
+    
     func handleEvent(forRegion region: CLRegion!) {
         
         // customize your notification content
