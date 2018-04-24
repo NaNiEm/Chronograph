22487d2852dc0695a57a027f7325c7bb191af4ad
NancyGomez <nagomez@csumb.edu>
2018-04-24 01:35:25 -0700

    Added image picker to profile page and edit button to indicate the items are editable

--- ChronographApp/ChronographApp/ViewControllers/ProfileViewController.swift
+++ ChronographApp/ChronographApp/ViewControllers/ProfileViewController.swift
@@ -8,17 +8,75 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileLocation: UILabel!
+    @IBOutlet weak var profileRecent: UITableView!
+    
+    // Instantiate a UIImagePickerController
+    var vc : UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onImageTap(_ sender: Any) {
+        chooseImage()
+    }
+    func chooseImage() {
+        vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    // helper function
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRect(0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+    // Implement the delegate method
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = resize(image: originalImage, newSize: CGSize(width: 300, height: 300))
+        
+        // Do something with the images (based on your use case)
+        profileImg.image = editedImage
+        // TODO: set image view to edited image or something like that
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
     }
 
+}
+// helper
+extension CGRect {
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        self.init(x:x, y:y, width:w, height:h)
+    }
 }
